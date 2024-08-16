527528585e60b60179bdcbc203ba86fecdd844d7
Muhammad Hamza <45312947+thehamzza@users.noreply.github.com>
2024-08-16 17:16:52 +0500

    code commit

---
+++ transactions/serializers.py
@@ -0,0 +1,9 @@
+# transactions/serializers.py
+
+from rest_framework import serializers
+from .models import Transaction
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
